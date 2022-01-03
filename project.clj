e263d18dfbe15a2235bc6814609f5da02cf7dd5f
Ken Smith <ken.smith@2scientists.net>
2022-01-03 16:28:37 -0700

    Project setup

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "Personal solutions to Advent of Code 2016 puzzles"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
-  :repl-options {:init-ns advent2016.core})
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [advent-utils "0.1.3"]]
+  :repl-options {:init-ns advent2016.day01})
