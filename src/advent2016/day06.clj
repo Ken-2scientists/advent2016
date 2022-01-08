3ebcc0c48e2ff5c38a74cdc167334cb1f9686fdf
Ken Smith <ken.smith@2scientists.net>
2022-01-08 15:08:09 -0700

    Day06 Solutions

---
+++ src/advent2016/day06.clj
@@ -0,0 +1,25 @@
+(ns advent2016.day06
+  (:require [advent-utils.core :as u]))
+
+(def day06-input (u/puzzle-input "day06-input.txt"))
+
+(defn frequent-chars
+  [max-or-min-f input]
+  (let [size (count input)]
+    (->> (apply interleave input)
+         (partition size)
+         (map frequencies)
+         (map (partial apply max-or-min-f val))
+         (map first)
+         (apply str))))
+
+(def most-frequent-chars (partial frequent-chars max-key))
+(def least-frequent-chars (partial frequent-chars min-key))
+
+(defn day06-part1-soln
+  []
+  (most-frequent-chars day06-input))
+
+(defn day06-part2-soln
+  []
+  (least-frequent-chars day06-input))
